2f0cbb5caa714198b1d0438457480ac489710f48
Tyler <bogotanorte1719@gmail.com>
2022-12-03 12:39:20 -0700

    Add search field for front end and backend will send response

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    
+    <div class="search">
+      <p>Enter a movie title:</p>
+      <input v-model="search" placeholder="edit me" />
+      <button @click="doSearch()">Search</button>
+    </div>
   </div>
 </template>
 
@@ -9,7 +13,22 @@
 
 export default {
   name: 'Home',
-  
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    async doSearch() {
+      console.log(this.search);
+      await fetch('http://localhost:3000/movies?search='+this.search, {mode: 'cors'})
+            .then((result) => result.json())
+            .then((data) => {
+                console.log(data);
+                
+            }).catch(err => console.error(err));
+    }
+  }
 }
 </script>
 
